65ce9c0c69c2d6d66a2acd2a725e88dd2b6bce5f
Dillon Forestell <djlf729@gmail.com>
2017-10-03 21:11:33 -0500

    add validations for presense of time date address and host team on the game class

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,5 @@ class Game < ActiveRecord::Base
 	belongs_to :host, class_name: :User
 	belongs_to :traveler, class_name: :User
 
+	validates :time, :date, :field_address, :host_id, presence: true
 end
