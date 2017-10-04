f33469d38a1598af2922c6e3f7eee1df434eb79b
Dillon Forestell <djlf729@gmail.com>
2017-10-04 14:44:33 -0500

    add a method for new create and show to the user controller with basic logic for new and create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,1 +1,21 @@
-users_controller.rb
+class UsersController < ApplicationController
+
+	def new 
+		@user = User.new
+	end
+
+	def create 
+		@user = User.new(registration_params)
+		if @user.save
+			session[:id] = @user.id
+			redirect to 'games/available'
+		else
+			render 'new'
+		end
+	end
+
+	def show
+
+	end
+
+end	
