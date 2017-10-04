66e6fda30ee1e0101c6cc63f67bee0092d54e151
Dillon Forestell <djlf729@gmail.com>
2017-10-04 14:19:46 -0500

    create restful routes for users and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+	resources :users, only: [:new, :create, :show]
+	resources :sessions, only: [:new, :create, :destroy]
 	resources :games, only: [:new, :show, :create, :destroy]
 	get '/games/available' => 'games#index'
 end
