0235b360e568dccce5e18045f4d1c3bf4e5fb649
Dillon Forestell <djlf729@gmail.com>
2017-10-07 12:41:05 -0500

    add a class method to the games class to be able to differentiate between games that are in the future and ones that have already passed

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,8 @@ class Game < ActiveRecord::Base
 	belongs_to :traveler, class_name: :User, optional: true
 
 	validates :time, :date, :field_address, presence: true
+
+	def self.future
+    where("starts_at > ?", Time.current)
+  end
 end
