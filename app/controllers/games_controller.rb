07049ab4bc3876cfb6078da4b4ccd9267c16530c
Dillon Forestell <djlf729@gmail.com>
2017-10-07 12:34:36 -0500

    make a create method in the games controller, and get rid of the automatic validation from the belongs to association for traveler

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,4 +4,21 @@ class GamesController < ApplicationController
 		@game = Game.new
 	end
 
+	def create
+		authenticate!
+		@game = Game.new(new_game)
+		@game.host_id = current_user.id
+
+		if @game.save
+			redirect_to @game 
+		else
+			redirect_to root_path
+		end
+	end
+
 end
+
+private
+	def new_game
+		params.require(:game).permit(:time, :date, :field_address)
+	end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ActiveRecord::Base
 	belongs_to :host, class_name: :User
-	belongs_to :traveler, class_name: :User
+	belongs_to :traveler, class_name: :User, optional: true
 
-	validates :time, :date, :field_address, :host_id, presence: true
+	validates :time, :date, :field_address, presence: true
 end
