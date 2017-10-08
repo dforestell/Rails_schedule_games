cb6ed8d08a259b2c0e953802a0ac3121c147bbe5
Dillon Forestell <djlf729@gmail.com>
2017-10-08 08:27:52 -0500

    play around with errors and flash messages, ultimately not make a decision yet

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ActiveRecord::Base
 	has_many :hosted_games, class_name: :Game, foreign_key: :host_id
 	has_many :away_games, class_name: :Game, foreign_key: :traveler_id
 
+	validates :full_name, :email, :team_name, :password, presence: true
+	validates :email, uniqueness: true
+
 	has_secure_password
 end
