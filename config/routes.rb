4685e4fe553527d2b76af085e399df79775b64ad
Dillon Forestell <djlf729@gmail.com>
2017-10-18 18:34:30 -0500

    change root bath to a simple welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-	root 'users#new'
+	root 'users#index'
+	get '/', to: 'users#index'
 	resources :users, only: [:new, :create, :show]
 	resources :sessions, only: [:new, :create, :destroy]
 	resources :games, except: [:edit]
